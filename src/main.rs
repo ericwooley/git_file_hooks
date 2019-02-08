65e08eab320b75509043cd551f1da82f31b1d20a
Eric Wooley <ericwooley@gmail.com>
2019-02-08 15:50:30 -0800

    run git commands

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,21 @@ extern crate glob;
 extern crate serde;
 extern crate serde_yaml;
 use std::process;
-mod command;
+mod git_command;
 mod config;
 mod parse;
 
 fn main() {
     let conf = config::from_file();
-    let hook = command::get_hook();
+    let hook = git_command::get_hook();
     println!("Running hook: {}", hook);
-    let commands = match conf.get_commands(&hook) {
+    let commands = match conf.get_commands(&hook) { 
         Some(c) => c,
         None => {
             println!("No commands for: {}", hook);
             process::exit(0);
         }
     };
-    let files = command::get_changed_files();
+    let files = git_command::get_changed_files();
     // command::run_commands(hook, files)
 }
